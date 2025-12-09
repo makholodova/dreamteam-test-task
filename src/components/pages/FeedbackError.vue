b93ccf26cf0f32dd2ccb35df0bc40bc421da3ef7
Marina Kholodova <marinaholodova@gmail.com>
2025-12-09 14:54:39 +0300

    feat: add pages for error and success message

---
+++ src/components/pages/FeedbackError.vue
@@ -0,0 +1,58 @@
+﻿<script setup>
+import ErrorIcon from "../../assets/icons/error-icon.svg";
+import BaseButton from "@/components/ui/BaseButton.vue";
+import FeedbackHeader from "@/components/feedback/FeedbackHeader.vue";
+
+const emit = defineEmits(['cancel', 'retry'])
+</script>
+
+<template>
+  <div class="feedback-wrapper">
+    <FeedbackHeader
+        subtitle="Попробуйте заполнить форму позже"
+        title="Что-то пошло не так"
+    />
+    <img :src="ErrorIcon" alt="Успешно" class="feedback-icon"/>
+    <div class="feedback__actions">
+      <BaseButton
+          text="Закрыть"
+          variant="secondary"
+          @click="emit('cancel')"/>
+      <BaseButton
+          text="Попробовать снова"
+          @click="emit('retry')"
+      />
+    </div>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/mixins.scss" as m;
+
+.feedback-wrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 95px;
+  @include m.phone {
+    gap: 44px;
+  }
+}
+
+.feedback-icon {
+  width: 100px;
+}
+
+.feedback__actions {
+  width: 100%;
+  display: flex;
+  gap: 28px;
+
+  @include m.phone {
+    flex-direction: column-reverse;
+    gap: 20px;
+  }
+}
+</style>
---
+++ src/components/pages/FeedbackSuccess.vue
@@ -0,0 +1,40 @@
+﻿<script setup>
+import SuccessIcon from "../../assets/icons/success-icon.svg";
+import BaseButton from "@/components/ui/BaseButton.vue";
+import FeedbackHeader from "@/components/feedback/FeedbackHeader.vue";
+
+const emit = defineEmits(['cancel'])
+</script>
+
+<template>
+  <div class="feedback-wrapper">
+    <FeedbackHeader
+        subtitle="Свяжемся с вами в ближайшее время"
+        title="Спасибо, ваш ответ отправлен"
+    />
+    <img :src="SuccessIcon" alt="Успешно" class="feedback-icon"/>
+    <BaseButton
+        text="На главную"
+        @click="emit('cancel')"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/mixins.scss" as m;
+
+.feedback-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 95px;
+  @include m.phone {
+    gap: 44px;
+  }
+}
+
+.feedback-icon {
+  width: 100px;
+}
+
+</style>
